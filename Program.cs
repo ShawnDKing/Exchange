b85ad3d40939329d1c9dd222105c7e443b1d9d4b
ShawnDKing <shawnking38061074@gmail.com>
2022-04-16 13:14:34 -0700

    Beginning of UI/Program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace Exchange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fromCurrency = " ";
+            string toCurrency = " ";
+            int total = 1;
+
+            Console.WriteLine("Welcome to the currency echange system");
+            Console.WriteLine("Available currencies are: (USD) US Dollar, (GBP) British Pound, (CAN) Canadian Dollar, or (EUR) Euro");
+
+            Console.WriteLine("Would you like to convert currency now? (Y or N)");
+
+            string answer = Console.ReadLine();
+            string choice = answer.ToUpper();
+            while (choice == "Y")
+            {
+                Console.WriteLine("Please select your currency you want to convert FROM (Example - USD): ");
+                fromCurrency = Console.ReadLine();
+
+                Console.WriteLine("Please select your currency you want to convert TO (Example - USD): ");
+                toCurrency = Console.ReadLine();
+
+
+
+                Console.WriteLine("The exchange rate in us dollars is: ");
+            }
+
+            Console.WriteLine("Your total currency in USD = " + total);
+            Console.WriteLine("Thank you for using the currency exchange.");
         }
     }
 }
